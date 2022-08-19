431689a619cd455120a81708cf12645e876f4631
vinhng2301 <ducvinhnguyen0201@gmail.com>
2022-08-19 14:03:16 +0700

    add service

---
+++ src/main/java/com/example/mvc_th_customer/service/ICustomerService.java
@@ -0,0 +1,17 @@
+package com.example.mvc_th_customer.service;
+
+import com.example.mvc_th_customer.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
